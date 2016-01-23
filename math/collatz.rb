742a85773bef6536e61a34115cb3ba5e1723c079
Alex Weisberger <alex.m.weisberger@gmail.com>
2016-01-23 12:22:46 -0500

    Pass rubocop.

--- math/collatz.rb
+++ math/collatz.rb
@@ -6,35 +6,26 @@ def next_in_chain(n)
   end
 end
 
+def calc_chain(start)
+  chain = []
+  while start != 1
+    start = next_in_chain start
+    chain << start
+  end
+  chain.length
+end
+
 def longest_chain(max)
   longest_chain_start = 1
   longest_chain = 1
   1.upto max do |i|
-    puts "Checking #{i}"
-
-    chain_length = 1
-    start = i
-    chain = []
-    while start != 1
-      chain_length += 1
-      start = next_in_chain start
-      chain << start
-    end
-
-    chain.each do |e|
-      print "#{e} -> "
-    end
-
-    puts
-    puts
-
-    if chain_length > longest_chain
+    len = calc_chain(i)
+    if len > longest_chain
       longest_chain_start = i
-      longest_chain = chain_length
+      longest_chain = len
     end
   end
-
   longest_chain_start
 end
 
-puts longest_chain ARGV[0].to_i
+puts longest_chain(ARGV[0].to_i)
