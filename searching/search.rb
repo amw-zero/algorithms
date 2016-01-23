9e08f36071f7c8aa482f0d5149d16a3ff4d59ab5
Alex Weisberger <alex.m.weisberger@gmail.com>
2016-01-23 12:34:26 -0500

    Rubocop cleanups.

--- searching/search.rb
+++ searching/search.rb
@@ -1,9 +1,11 @@
 # See if search element is the middle element of array (floor of midpoint)
 # If it is, that is the value
 # If it isn't, and the element is less than the middle element,
-#   call binary_search on the subarray from index 0 to the middle index (all lower numbers)
+#   call binary_search on the subarray from index 0 to the middle
+#   index (all lower numbers)
 # If it isn't, and the elemnt is greater than the middle element,
-#   call binary_search on the subarray from the middle index to the end of the array (all higher numbers)
+#   call binary_search on the subarray from the middle index to
+#   the end of the array (all higher numbers)
 
 def binary_search(arr, n, num_iterations)
   mid = arr.count / 2
--- sorting/merge_sort.rb
+++ sorting/merge_sort.rb
@@ -1,45 +1,42 @@
 def merge_sort_imperative(arr, depth = 0)
-  indent_print(arr, depth)
-  if arr.length > 1
-    mid	= arr.size / 2
-    left	= arr[0...mid]
-    right	= arr[mid..arr.size]
+  return if arr.length <= 1
 
-    merge_sort(left, depth + 1)
-    merge_sort(right, depth + 1)
+  mid	= arr.size / 2
+  left	= arr[0...mid]
+  right	= arr[mid..arr.size]
 
-    puts "Merging #{left}  --  #{right}"
-    li = 0
-    ri = 0
-    ai = 0
-    while li < left.size && ri < right.size
-      if left[li] < right[ri]
-        puts 'taking left'
-        arr[ai] = left[li]
-        li += 1
-      else
-        puts 'taking right'
-        arr[ai] = right[ri]
-        ri += 1
-      end
-      ai += 1
-    end
+  merge_sort_imperative(left, depth + 1)
+  merge_sort_imperative(right, depth + 1)
 
-    while li < left.size
-      puts 'taking left overflow'
+  puts "Merging #{left}  --  #{right}"
+  li = 0
+  ri = 0
+  ai = 0
+  while li < left.size && ri < right.size
+    if left[li] < right[ri]
+      puts 'taking left'
       arr[ai] = left[li]
       li += 1
-      ai += 1
-    end
-
-    while ri < right.size
-      puts 'taking right overflow'
+    else
+      puts 'taking right'
       arr[ai] = right[ri]
       ri += 1
-      ai += 1
     end
+    ai += 1
+  end
 
-    puts "Done #{arr}"
+  while li < left.size
+    puts 'taking left overflow'
+    arr[ai] = left[li]
+    li += 1
+    ai += 1
+  end
+
+  while ri < right.size
+    puts 'taking right overflow'
+    arr[ai] = right[ri]
+    ri += 1
+    ai += 1
   end
 end
 
